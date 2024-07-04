756c6dd37489e7fe7de2edd871718b151cd028e2
edu1ardo1 <167472890+edu1ardo1@users.noreply.github.com>
2024-07-04 18:08:52 -0400

    Add files via upload

---
+++ JOVANNI_RIQUELME_008D.py
@@ -0,0 +1,8 @@
+import os
+import time
+#desarolle un sistema que permita registrar los pedidos
+csv = open
+random = 1,1000
+print("ingrese su nombre y apellido")
+input("")
+print("bienvenido a CatPremiun")
